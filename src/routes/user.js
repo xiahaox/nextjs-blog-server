13e4e0f4c9b17aea5eac3ba75a27d09f0cbf661e
xiahaox <1043911346@qq.com>
2024-01-23 17:47:17 +0800

    feat: mysql接入 v2

---
+++ src/routes/user.js
@@ -0,0 +1,10 @@
+// const router = require('koa-router')();
+
+// const { getList, updateUser, delete: del } = require('../controller/user');
+
+// router
+//     .get('/list', getList) // 获取列表
+//     .put('/:userId', updateUser) // 更新用户信息
+//     .delete('/:userId', del) // 删除用户
+
+// module.exports = router
